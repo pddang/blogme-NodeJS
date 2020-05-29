f630c40d1b695610cc2e5346e6a9708a1bb71518
Phan Dang <philip.ddang>
2020-05-28 23:45:42 -0700

    added User model

---
+++ routes/auth.js
@@ -0,0 +1,22 @@
+var jwt = require('express-jwt');
+var secret = require('../config').secret;
+
+function getTokenFromHeader(req){
+    if(req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Token'){
+        return req.headers.authorization.split(' ')[1];
+    }
+
+    return null;
+
+}
+
+var auth = {
+    required: jwt({
+        secret: secret,
+        userProperty:'payload',
+        credentialsRequired: false,
+        getToken: getTokenFromHeader
+    })
+};
+
+module.exports = auth;
